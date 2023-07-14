fcc62d1acaa7ed47fa9fe80f84f5096436a8717f
Huda Prasetyo <real.hudaprasetyo@gmail.com>
2023-07-14 20:48:32 +0700

    change wording in login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,7 +32,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="#"><b>Manajemen</b>Barang</a>
+    <a href="#"><b>Item</b>Management</a>
   </div>
   <!-- /.login-logo -->
   <div class="card">
